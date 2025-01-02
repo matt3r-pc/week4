71f0540e99d45fe867a4fc7bfe79d462107d4885
Marat Muhhametzjanov <marat.muhhametzjanov2004@tktk.ee>
2025-01-02 15:37:35 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; //bool - true/false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1,4);
+    //Console.WriteLine($"cpu has generated{cpuRandom}.");
+    Console.WriteLine("make a guess. enter a number 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("congratulations, you won!");
+        loopActive = false;
+    }
+    else
+    {
+        Console.WriteLine("oops, try again");
+    }
+}
+Console.WriteLine("have a nice day");  
